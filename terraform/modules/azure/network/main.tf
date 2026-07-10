47202a41df93d4140c04653482a6bf3d6fe3ff75
Sundeep Kumar <ashoksundeep@gmail.com>
2026-07-10 17:52:57 +0000

    terraform: implement Azure network security and routing

--- terraform/modules/azure/network/main.tf
+++ terraform/modules/azure/network/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_virtual_network" "this" {
 # AKS Subnet
 #########################################################
 
-resource "azurerm_subnet" aks {
+resource "azurerm_subnet" "aks" {
 
     name                 = var.aks_subnet_name
     resource_group_name  = azurerm_resource_group.this.name
@@ -71,5 +71,112 @@ resource "azurerm_subnet" "bastion" {
     address_prefixes = [
         var.bastion_subnet_prefix
     ] 
-    
+
 }
+
+############################################################################################
+# AKS Network Security Group [local.tf is where the local.resource.prefix comes from]
+############################################################################################
+
+resource "azurerm_network_security_group" "aks" {
+
+    name                = "${local.resource_prefix}-aks-nsg"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.this.name
+    tags                = var.tags
+
+}
+
+############################################################################
+# Shared services nsg
+###########################################################################
+
+resource "azurerm_network_security_group" "shared" {
+
+    name                = "${local.resource_prefix}-shared-nsg"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.this.name
+    tags                = var.tags
+
+}
+
+############################################################################
+# Bastion nsg
+############################################################################
+
+resource "azurerm_network_security_group" "bastion" {
+    
+    name                = "${local.resource_prefix}-bastion-nsg"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.this.name
+    tags                = var.tags
+
+}
+
+############################################################################
+# AKS NSG association
+############################################################################
+
+resource "azurerm_subnet_network_security_group_association" "aks" {
+    subnet_id                 = azurerm_subnet.aks.id
+    network_security_group_id = azurerm_network_security_group.aks.id
+}
+
+############################################################################
+# Shared NSG association
+############################################################################
+
+resource "azurerm_subnet_network_security_group_association" "shared" {
+    subnet_id                 = azurerm_subnet.shared.id
+    network_security_group_id = azurerm_network_security_group.shared.id
+}
+
+#############################################################################
+# Bastion NSG association
+#############################################################################
+
+resource "azurerm_subnet_network_security_grup_association" "bastion" {
+    subnet_id                 = azurerm_subnet.bastion.id
+    network_security_group_id = azurerm_network_security_group.bastion.id
+}
+
+#############################################################################
+# Route table
+#############################################################################
+
+resource "azure_route_table" "this" {
+    name                = "${local.resource_prefix}-rt
+    location            = var.location
+    resource_group_name = azurerm_resource_group.this.name
+    tags                = var.tags
+}
+
+##############################################################################
+# Associate AKS route table
+##############################################################################
+
+resource "azurerm_subnet_route_table_association" "aks" {
+    subnet_id      = azurerm_subnet.aks.id
+    route_table_id = azurerm_route_table.this.id
+}
+
+#################################################################################
+# Associate route table to shared services
+#################################################################################
+
+resource "azurerm_subnet_route_table_association" "shared" {
+    subnet_id      = azurerm_subnet.shared.id
+    route_table_id = azurerm_route_table.this.id
+}
+
+#################################################################################
+# Associate route table to bastion
+#################################################################################
+
+resource "azurerm_subnet_route_table_association" "bastion" {
+    subnet_id      = azurerm_subnet.bastion.id
+    route_table_id = azurerm_route_table.this.id
+}
+
+ 
+
